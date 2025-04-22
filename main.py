b7dcf7b91b6f49f865877c1a86baa61dbfeab14b
Gustavo Cavalcante <144056595+guscavalcante-dev@users.noreply.github.com>
2025-04-22 12:07:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ async def scrape_film_page(client, film_name, url):
 
 
 async def main():
-    roll_dice()
+    roll_dice(6, 2)
     async with httpx.AsyncClient(http2=True) as client:
         film_urls = await get_all_film_urls(client)
         print(f"🎞️ {len(film_urls)} filmes encontrados.\n")
